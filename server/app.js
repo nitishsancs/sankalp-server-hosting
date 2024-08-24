1e098be000e03060a59a03179d0be5f454dc8f9d
nitishsancs <nitishsancs@gmail.com>
2024-08-24 11:09:28 +0530

    server-no no

--- server/app.js
+++ server/app.js
@@ -400,7 +400,7 @@ const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 
 // Add CORS headers middleware
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://sankalp-client-hosting-hvbeluf1c-nitishsancs-projects.vercel.app");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
